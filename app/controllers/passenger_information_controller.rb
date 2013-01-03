d4ec62ac6f3c93ade053b171feecdc5cc74fa9ea
Tiffany Wang <tiffanywang14@berkeley.edu>
2013-01-03 01:45:58 -0800

    error message on blank fields

--- app/controllers/passenger_information_controller.rb
+++ app/controllers/passenger_information_controller.rb
@@ -4,9 +4,18 @@ class PassengerInformationController < ApplicationController
   end
 
   def check_in
-		passenger_info = PassengerInformation.new(params[:passenger_info])
-    passenger_info.check_in
-    flash[:notice] = "You've done your part.  We'll do the rest.  We'll check you in and email your boarding pass to you. Refresh or leave this page at your leisure!"
-    redirect_to root_path
+    passenger_info = params[:passenger_info]
+    flash[:error] = ''
+    passenger_info.keys.each do |field|
+      flash[:error] += ', ' + field.gsub('_', ' ') if passenger_info[field].blank?
+    end
+    if not flash[:error].blank?
+      flash[:error] = 'You left the following fields blank: ' + flash[:error][2..-1] + '.'
+    else
+      passenger_info = PassengerInformation.new(passenger_info)
+      passenger_info.check_in
+      flash[:notice] = "We'll check you in and email your boarding pass to you. Refresh or leave this page at your leisure!"
+    end  
+  redirect_to root_path
   end
 end
