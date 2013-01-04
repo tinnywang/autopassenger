71cceffba59a341596e9c14f3bf21e4876646bd1
Tiffany Wang <tiffanywang14@berkeley.edu>
2013-01-04 00:41:27 -0800

    heroku troubleshooting

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module Autopassenger
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # Don't connect to the database as part of rake assets:precompile.
+    config.assets.initialize_on_precompile = false
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
